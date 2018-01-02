a2c9d884e3093958b618c44805d0b7198a02755c
hujun100 <100227557@qq.com>
2018-01-02 16:25:09 +0800

    Update config.py

--- train_test/config.py
+++ train_test/config.py
@@ -3,17 +3,27 @@ import random
 import numpy as np
 import cv2
 import math
-## data_root_dir + each line of data_list should be the path of an image
+## data_root_dir + each line of data_list should be the path of an image and label
+###for example:
+###data_root_dir = 'data/'
+###lines in data_list
+### 1.jpg 1  
+### 2.jpg 2
+### Then data/1.jpg and data/2.jpg must exist
 data_root_dir = 'data/'
 data_list = 'txt/webface_list.txt' 
 
 BATCH_SIZE = 100
+###m_value = 1 means SINGLE in sphereface-caffe
+###m_value = 2 means DOUBLE in sphereface-caffe
+###And so on
 M_VALUE = 4
 
 basic_learning_rate = 0.1
 weight_decay = 0.0005
 step_value = [16000, 20000, 24000]
 max_iter = 28000
+### factor means gamma in sphereface-caffe when use "multistep" learning  rate
 factor = 0.1
 
 lambda_base = 1000
